2f89723b3039a985774ba10361130763ddc8ca76
intenseG <intenseg1515@gmail.com>
2021-06-17 12:22:10 +0900

    タブメニューの実装（クラス未使用）

---
+++ script.js
@@ -0,0 +1,21 @@
+let tabNum = 0;
+
+let tabElms = document.getElementsByClassName('tab');
+
+for (let i = 0; i < tabElms.length; i++) {
+    tabElms[i].addEventListener('click', switchMenu);
+}
+
+let contentElms = document.getElementsByClassName('content');
+
+function switchMenu() {
+    document.getElementsByClassName('active')[0].classList.remove('active');
+    this.classList.add('active');
+
+    document.getElementsByClassName('show')[0].classList.remove('show');
+
+    const tabArray = Array.from(tabElms);
+    const index = tabArray.indexOf(this);
+    contentElms[index].classList.add('show');
+}
+
